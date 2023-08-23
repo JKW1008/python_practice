2ddacc5c6b95b43c1e0e425f609343a84c1a4472
JKW1008 <todendvv91@gmail.com>
2023-08-23 16:07:30 +0900

    test

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from numpy import logical_not
 import pandas as pd
 acc = pd.Series([1, 2, 3, 4, 5])
 acc += 10
 print(acc)
+
+
+url = 'https://www.dip.or.kr'
+response = requests.get(url)
+html = bs(response.text, 'html.parser')
+logo = html.find('img')
+print(logo)
